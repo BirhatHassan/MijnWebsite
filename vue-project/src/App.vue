d6ab2414c2d272a4f926edd42fc06eb35795c774
BirhatHassan <birhat.hassan@hva.nl>
2025-09-25 23:12:17 +0200

    initialize Vue project with essential files and configurations

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <nav-bar />
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
